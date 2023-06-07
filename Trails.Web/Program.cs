3a21bc48c7444c5b1a3063caf97e5c97c15ffd13
russ4stall <russ@slashpine.tech>
2023-06-07 17:30:47 -0400

    yup

--- Trails.Web/Program.cs
+++ Trails.Web/Program.cs
@@ -1,32 +1,33 @@
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Trails.Domain;
 
 var  myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: myAllowSpecificOrigins,
-        policy  =>
-        {
-            policy.WithOrigins("https://playground.apps.fourscorepicks.com")
-                .AllowAnyHeader().
-                AllowAnyMethod();
-        });
-});
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy(name: myAllowSpecificOrigins,
+//         policy  =>
+//         {
+//             policy.WithOrigins("https://playground.apps.fourscorepicks.com")
+//                 .AllowAnyHeader().
+//                 AllowAnyMethod();
+//         });
+// });
 
 
 builder.Services.AddDbContext<TrailsDb>(opt => opt.UseInMemoryDatabase("Trails"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Configure middleware to add X-Forwarded-For and X-Forwarded-Proto headers
-// builder.Services.Configure<ForwardedHeadersOptions>(options =>
-// {
-//     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-//     //accept all networks and proxies
-//     options.KnownNetworks.Clear();
-//     options.KnownProxies.Clear();
-// });
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    //accept all networks and proxies
+    options.KnownNetworks.Clear();
+    options.KnownProxies.Clear();
+});
 
 var flTrailsUri = new Uri("https://www.fdacs.gov/Forest-Wildfire/Our-Forests/State-Forests/State-Forest-Recreation/Recreational-Activities/Hiking/Trailwalker-Program/Trailwalker-Trail-List-by-State-Forest");
 builder.Services.AddSingleton<ITrailsWebScraper>(x => 
@@ -34,7 +35,7 @@ builder.Services.AddSingleton<ITrailsWebScraper>(x =>
 
 var app = builder.Build();
 
-//app.UseForwardedHeaders();
+app.UseForwardedHeaders();
 
 app.MapGet("/trails", async (TrailsDb db) => await db.Trails.ToListAsync());
 
@@ -58,5 +59,5 @@ app.MapPost("/trails/sync", async (TrailsDb db, ITrailsWebScraper scraper) =>
 });
 
 
-app.UseCors(myAllowSpecificOrigins);
+// app.UseCors(myAllowSpecificOrigins);
 app.Run();
