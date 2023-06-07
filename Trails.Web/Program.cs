746d68f274a0d48553cc22aa5a1fb6f1c38e6d8d
russ4stall <russ@slashpine.tech>
2023-06-07 16:10:27 -0400

    cors troubleshooting

--- Trails.Web/Program.cs
+++ Trails.Web/Program.cs
@@ -2,18 +2,30 @@ using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Trails.Domain;
 
+var  myAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: myAllowSpecificOrigins,
+        policy  =>
+        {
+            policy.WithOrigins("https://playground.apps.fourscorepicks.com");
+        });
+});
+
+
 builder.Services.AddDbContext<TrailsDb>(opt => opt.UseInMemoryDatabase("Trails"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Configure middleware to add X-Forwarded-For and X-Forwarded-Proto headers
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    //accept all networks and proxies
-    options.KnownNetworks.Clear();
-    options.KnownProxies.Clear();
-});
+// builder.Services.Configure<ForwardedHeadersOptions>(options =>
+// {
+//     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+//     //accept all networks and proxies
+//     options.KnownNetworks.Clear();
+//     options.KnownProxies.Clear();
+// });
 
 var flTrailsUri = new Uri("https://www.fdacs.gov/Forest-Wildfire/Our-Forests/State-Forests/State-Forest-Recreation/Recreational-Activities/Hiking/Trailwalker-Program/Trailwalker-Trail-List-by-State-Forest");
 builder.Services.AddSingleton<ITrailsWebScraper>(x => 
@@ -24,18 +36,6 @@ var app = builder.Build();
 app.UseForwardedHeaders();
 
 app.MapGet("/trails", async (TrailsDb db) => await db.Trails.ToListAsync());
-
-app.MapGet("/test", async () => {
-
-    using (var client = new HttpClient())
-    {
-        HttpResponseMessage response = new HttpResponseMessage();
-
-        response = await client.GetAsync("https://www.google.com/").ConfigureAwait(false);
-
-        return response.Content.ReadAsStringAsync().Result;
-    }
-});
 
 app.MapPost("/trails/sync", async (TrailsDb db, ITrailsWebScraper scraper) =>
 {
